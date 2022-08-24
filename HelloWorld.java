592314630fcb2594d68a04b33783d6a8bffb41bb
Hrithik Agarwal <hrithikinfinite@gmail.com>
2022-08-24 14:46:43 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 
 public class HelloWorld {
-	public static void main(String[] args) {		System.out.println("Hello WOrld!!!");
+	public static void main(String[] args) {
+		System.out.println("Hello WOrld!!!");
+		System.out.println("Hello WOrld2!!!");
+		System.out.println("Hello WOrld!3!!");
+		System.out.println("Hello WOrld!!4!");
 	}
 }
